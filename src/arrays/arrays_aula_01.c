b7c32b39543807406fc39ea1ff02756b0e1d85ec
Kevin Meireles <105472882+Kebions2077@users.noreply.github.com>
2025-10-27 00:03:56 -0300

    Implimentação usando alocação dinamica
    Modifica a alocação estatica do vetor para a alocação dinamica usando um ponteiro que é criado usando o tamanho digitado como referencia, criando tambem uma função que recebe por referencia o tamanho e o ponteiro

--- src/arrays/arrays_aula_01.c
+++ src/arrays/arrays_aula_01.c
@@ -1,27 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Este programa lê uma quantidade de notas informada pelo usuário,
-// calcula a média dessas notas e exibe o resultado na tela.
-float calcularMedia(int qtd){
-    float notas[qtd];
-    float soma = 0;
-
-    for(int i=0; i< qtd; i++){
-        printf("Digite a nota %d: ", i+1);
-        scanf("%f", &notas[i]);
-        soma += notas[i];
+float cal(float *p, int n){
+    float m;
+    m=0;
+    for(int y=0;y<n;y++){
+        m+= p[y];
     }
-
-    return soma / qtd;
+    return m/n;
+}
+void clear() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
 }
 
-int main(){
-    int qtd = 0;
-    printf("Digite a quantidade de notas que você quer saber a média: ");
-    scanf("%d", &qtd);
+int main(void){
+    int n,x;
+    n=x=0;
+    float *p,media;
+    p=NULL;
+    printf("-----------------Calcular Media-----------------\n");
+    printf("Digite quantas notas deseja para fazer a media:\n");
+     printf("-------------------------------------------------\n");
+    scanf("%i",&n);
+    p=malloc(n*sizeof(float));
+    if(p==NULL){
+        fprintf(stderr,"Erro De Memoria");
+        exit(1);
+    }
+    clear();
 
-    float media = calcularMedia(qtd);
-    printf("A média das notas é: %.2f\n", media);
+   while(x!=n){
 
+        printf("-----------------------\n");
+        printf("Digite a nota:\n");
+        scanf("%f",&p[x]);
+        printf("-----------------------\n");
+        x++;
+
+
+
+
+    }
+    media=cal(p,n);
+    clear();
+    printf("Sua Media:%.2f",media);
+    free(p);
     return 0;
 }
